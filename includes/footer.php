4bd3593191f8b58d04749bd56af366857e30b4c0
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-21 16:29:57 +0100

    modification des styles pour le burger, footer, header

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,13 @@
 <footer>
+    <div class="footer_container">
+    <div class="footer_phone">
+        <p> 📞  +33 6 12 34 56 78</p>
+    </div>
+     <div class="footer_logo-maps">
+        <img src="./assets/logo_google-maps.png" alt="logo google maps">
+     </div>
+    </div>
+    <div class="footer_end">
     <p>&copy; <?php echo date('Y'); ?> - Coffice</p>
+    </div>
 </footer>
-</body>
-</html>
