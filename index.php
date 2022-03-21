5d4d23e1522c9a2d581077af3c4fdf067f79955d
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-21 14:25:43 +0100

    modification de l'index.php

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
+require_once './functions/autoLoadFunction.php';
+session_start();
+date_default_timezone_set('Europe/Paris');
+setlocale(LC_ALL, 'fr_FR', 'fr', 'FR', 'fr_FR@euro');
+
+spl_autoload_register(function ($className) {
+    include './classes/' . $className . '.php';
+});
+
+require_once './includes/head.php';
+require_once './includes/main.php';
+require_once './includes/footer.php';
+
 
