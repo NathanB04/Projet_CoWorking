56ece24dcd94343c6274cd8752e256cfb0b3aadb
Maël Barbé <mael.barbe47@gmail.com>
2022-03-21 17:47:51 +0100

    correction du footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,10 +4,7 @@
         <p> 📞  +33 6 12 34 56 78</p>
     </div>
      <div class="footer_logo-maps">
-        <img src="./assets/logo_google-maps.png" alt="logo google maps">
+        <img src="./assets/img/logo_google-maps.png" alt="logo google maps">
      </div>
-    </div>
-    <div class="footer_end">
-    <p>&copy; <?php echo date('Y'); ?> - Coffice</p>
     </div>
 </footer>
