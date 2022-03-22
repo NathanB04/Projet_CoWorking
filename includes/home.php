ebd4018e71fca75fd9e39e20fd9240a3cd62d63f
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-22 14:53:19 +0100

    style de la page home desktop

--- includes/home.php
+++ includes/home.php
@@ -1,3 +1,15 @@
+<header>
+<nav role="navigation">
+        <ul>
+            <li><a href="index.php?page=home">Home</a></li>
+            <li><a href="">Connexion</a></li>
+            <li><a href="">Etablissement</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
 <div class="homepage">
 <img src="assets/img/coffice_homepage.jpeg">
 </div>
+
