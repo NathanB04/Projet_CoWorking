df5d6cbb9af8001b87a3812cb1eeba5755df3f22
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-22 16:46:39 +0100

    modifications mineures sur la page d'accueil

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -1,3 +1,2 @@
 <?php
 include 'home.php';
-include 'nav.php';
--- includes/home.php
+++ includes/home.php
@@ -3,13 +3,32 @@
         <ul>
             <li><a href="index.php?page=home">Home</a></li>
             <li><a href="">Connexion</a></li>
-            <li><a href="">Etablissement</a></li>
+            <li><a href="index.php?page=etablissement">Etablissement</a></li>
             <li><a href="">Contact</a></li>
         </ul>
     </nav>
 </header>
 
-<div class="homepage">
-<img src="assets/img/coffice_homepage.jpeg">
-</div>
+<div class="container">
+    <div class="homepage">
+        <img src="assets/img/coffice_homepage.jpeg">
+    </div>
+    <div class="search-bar">
+     <form action="/action_page.php">
+         <label for="lieu">Choisissez un établissement :</label>
+             <select id="lieu" name="lieu">
+                <option value="cafe">Café</option>
+                <option value="restaurant">Restaurant</option>
+            </select>
+            <input id="searchTextField" type="text" size="50">
+        <input type="submit">
+    </form>
+    </div>
+    <div class="icons"> 
+    <img src="assets/img/icons-facebook.png">
+    <img src="assets/img/Instagram-Icon.png">
+    <img src="assets/img/twitter-icon.png"> 
+    </div>
+    <script src="js/maps.js"></script>
 
+</div>
