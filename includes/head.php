0b29dd856f0d9e07f453e1c27cbbd86dee8b7a46
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-22 16:46:02 +0100

    ajout d'une api google maps

--- includes/head.php
+++ includes/head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/style.css" />
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
     <title>Coffice</title>
 </head>
 <body>
