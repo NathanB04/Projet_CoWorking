f004edeb4e7a4de0717eba6c558720a9a7fdc3e2
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-23 11:19:10 +0100

    essai insertion autocomplete maps

--- js/maps.js
+++ js/maps.js
@@ -4,3 +4,20 @@ function initialize() {
   }
   
   google.maps.event.addDomListener(window, 'load', initialize);
+
+
+  <script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyByJWvJBCuJUdyw-5k1-rohTCCbGCPc65o&libraries=places&callback=initMap">
+</script>
+
+let autocomplete;
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById('autcomplete'),
+    {
+      types: ['establishment'],
+      compomentRestrictions: {'country': ['AU']},
+      fields: ['place_id', 'geometry', 'name']
+    }
+  );
+}
