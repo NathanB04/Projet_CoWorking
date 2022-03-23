b8c1f2d5d942c8c287d77031b52eb8f76f53cec4
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-23 11:19:40 +0100

    style page accueil

--- includes/home.php
+++ includes/home.php
@@ -16,19 +16,21 @@
     <div class="search-bar">
      <form action="/action_page.php">
          <label for="lieu">Choisissez un établissement :</label>
+         <br>
              <select id="lieu" name="lieu">
                 <option value="cafe">Café</option>
                 <option value="restaurant">Restaurant</option>
             </select>
-            <input id="searchTextField" type="text" size="50">
-        <input type="submit">
+            <input id="searchTextField" type="text" size="30" placeholder="Indiquez un lieu">
+        <input type="submit" style="width: 8rem; height: 30px; background-color: #0099cc; 
+        border-radius: 30px; color:white; font-weight:600">
     </form>
     </div>
-    <div class="icons"> 
-    <img src="assets/img/icons-facebook.png">
-    <img src="assets/img/Instagram-Icon.png">
-    <img src="assets/img/twitter-icon.png"> 
-    </div>
+     <div class="icons"> 
+        <img src="assets/img/icons-facebook.png">
+        <img src="assets/img/Instagram-Icon.png">
+        <img src="assets/img/twitter-icon.png"> 
+     </div>
     <script src="js/maps.js"></script>
 
 </div>
