08e4b0abab6ba3ad47761dab4bebf7aadf39f70f
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-29 17:31:56 +0200

    ajout des liens burger menu

--- includes/home.php
+++ includes/home.php
@@ -23,10 +23,10 @@
                 <span class="burger-bottom"></span>
             </div>
             <div class="burger-menu">
-                <a href="">Accueil</a>
+                <a href="http://cafe-wifi.fr">Accueil</a>
                 <a href="">Se connecter</a>
-                <a href="">Ajout d'un établissement</a>
-                <a href="">Liste des établissements</a>
+                <a href="http://cafe-wifi.fr/index.php?page=etablissement">Ajout d'un établissement</a>
+                <a href="http://cafe-wifi.fr/index.php?page=resultats">Liste des établissements</a>
             </div>
     </nav>
     </div>
