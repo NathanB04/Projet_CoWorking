294feadd852156fd76e710b5e80dae04ea7e97c3
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-29 17:31:45 +0200

    page renommée pour l'url

---
+++ includes/resultats.php
@@ -0,0 +1,102 @@
+<!-- nous sommes sur la page des résultats -->
+<div class="logo">
+    <a href="http://localhost/Projet_CoWorking/">
+        <img src="./assets/img/coffice_logo_blanc.png" alt="logo Coffice"> 
+    </a>
+</div>
+
+<body class="resultatsPage">
+
+<header>
+    <div class="navigation-desktop">
+        <nav role="navigation">
+            <ul>
+                <li><a href="index.php?page=home">Home</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a href="index.php?page=etablissement">Etablissement</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<nav>
+    <div class="burger-button">
+        <span class="burger-top"></span>
+        <span class="burger-middle"></span>
+        <span class="burger-bottom"></span>
+    </div>
+    <div class="burger-menu">
+        <a href="http://localhost:8888/Projet_CoWorking/index.php?page=home">Accueil</a>
+        <a href="">Se connecter</a>
+        <a href="">Ajout d'un établissement</a>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="product-details">
+        <h1>Australian café</h1>
+        <p class="information"><b>Description brève:</b> "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Cupiditate, atque molestiae velit nesciunt laborum odit eligendi minus ratione totam doloremque.</p>
+        <div class="control">
+            <button class="btn">
+                <span class="moreInfo">Plus d'info</span>
+            </button>
+        </div>
+    </div>
+    <div class="product-image">
+        <img src="./assets/img/cafe_coworking.jpg" alt="Image du café">
+    </div>
+</div>
+
+<div class="container">
+    <div class="product-details">
+        <h1>Australian café</h1>
+        <p class="information"><b>Description brève:</b> "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Cupiditate, atque molestiae velit nesciunt laborum odit eligendi minus ratione totam doloremque.</p>
+        <div class="control">
+            <button class="btn">
+                <span class="moreInfo">Plus d'info</span>
+            </button>
+        </div>
+    </div>
+    <div class="product-image">
+        <img src="./assets/img/cafe_coworking.jpg" alt="Image du café">
+    </div>
+</div>
+
+<div class="container">
+    <div class="product-details">
+        <h1>Australian café</h1>
+        <p class="information"><b>Description brève:</b> "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Cupiditate, atque molestiae velit nesciunt laborum odit eligendi minus ratione totam doloremque.</p>
+        <div class="control">
+            <button class="btn">
+                <span class="moreInfo">Plus d'info</span>
+            </button>
+        </div>
+    </div>
+    <div class="product-image">
+        <img src="./assets/img/cafe_coworking.jpg" alt="Image du café">
+    </div>
+</div>
+
+<div class="container">
+    <div class="product-details">
+        <h1>Australian café</h1>
+        <p class="information"><b>Description brève:</b> "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Cupiditate, atque molestiae velit nesciunt laborum odit eligendi minus ratione totam doloremque.</p>
+        <div class="control">
+            <button class="btn">
+                <span class="moreInfo">Plus d'info</span>
+            </button>
+        </div>
+    </div>
+    <div class="product-image">
+        <img src="./assets/img/cafe_coworking.jpg" alt="Image du café">
+    </div>
+</div>
+
+ <script src="js/burger.js"></script>
+ 
+ </body>
