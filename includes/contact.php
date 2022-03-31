e2d3e871e86100a4ed1fa48cf3f6a9a7c8bfd15f
Maël Barbé <mael.barbe47@gmail.com>
2022-03-31 11:19:28 +0200

    Création de la page contact

---
+++ includes/contact.php
@@ -0,0 +1,44 @@
+<div class="logo">
+    <a href="http://localhost/Projet_CoWorking/">
+        <img src="./assets/img/coffice_logo_blanc.png" alt="logo Coffice"> 
+    </a>
+</div>
+
+<body class="resultatsPage">
+
+<header>
+    <div class="navigation-desktop">
+        <nav role="navigation">
+            <ul>
+                <li><a href="index.php?page=home">Home</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a href="index.php?page=etablissement">Etablissement</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<nav>
+    <div class="burger-button">
+        <span class="burger-top"></span>
+        <span class="burger-middle"></span>
+        <span class="burger-bottom"></span>
+    </div>
+    <div class="burger-menu">
+        <a href="http://localhost:8888/Projet_CoWorking/index.php?page=home">Accueil</a>
+        <a href="">Se connecter</a>
+        <a href="">Ajout d'un établissement</a>
+    </div>
+</nav>
+
+<div class="email">
+    <p><o>E-mail :</o> lorem@lorem.com</p>
+</div>
+
+<div class="phone">
+    <p><o>Numéro de téléphone :</o> 00.00.00.00.00</p>
+</div>
+
+
+<script src="js/burger.js"></script>
+</body>
