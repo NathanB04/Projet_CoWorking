f6d8d2e6efa6ebb638d83d150f2ec82ea2c440a3
Maël Barbé <mael.barbe47@gmail.com>
2022-03-31 09:50:45 +0200

    Responsive de la page etablissement

--- includes/etablissement.php
+++ includes/etablissement.php
@@ -5,58 +5,66 @@
     </a>
 </div>
 
+<body class="body_etablissement">
+
+<header>
+    <div class="navigation-desktop">
+        <nav role="navigation">
+            <ul>
+                <li><a href="index.php?page=home">Home</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a href="index.php?page=etablissement">Etablissement</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
 <nav>
-            <div class="burger-button">
-                <span class="burger-top"></span>
-                <span class="burger-middle"></span>
-                <span class="burger-bottom"></span>
-            </div>
-            <div class="burger-menu">
-                <a href="http://cafe-wifi.fr">Accueil</a>
-                <a href="">Se connecter</a>
-                <a href="">Ajout d'un établissement</a>
-            </div>
+    <div class="burger-button">
+        <span class="burger-top"></span>
+        <span class="burger-middle"></span>
+        <span class="burger-bottom"></span>
+    </div>
+    <div class="burger-menu">
+        <a href="http://cafe-wifi.fr">Accueil</a>
+        <a href="">Se connecter</a>
+        <a href="">Ajout d'un établissement</a>
+    </div>
  </nav>
 
-
-
-
-<body class="body_etablissement">
-    <div class="body_container">
-        <div class="title-etablissement-page">
-            <h1>Nom de l'établissement</h1>
-        </div>
-        <div class="img-etablissement-page">
-            <img src="./assets/img/cafe_coworking.jpg" alt="Image du café">
-        </div>
-        <div class="select-horaire-etablissement-page">
-            <label for="day-select"><b><o>Choisissez un jour :</o></b></label>
-                <select name="horaire" id="horaire">
-                    <option value="lundi">Lundi 9H-17H</option>
-                    <option value="mardi">Mardi 9H-17H</option>
-                    <option value="mercredi">Mercredi 9H-17H</option>
-                    <option value="jeudi">Jeudi 9H-17H</option>
-                    <option value="vendredi">Vendredi 9H-17H</option>
-                </select>
-        </div>
-        <div class="adresse-etablissement-page">
-            <p><b><o>Adresse</o></b> : Lorem, ipsum dolor, 27140, Gisors.</p>
-        </div>
-        <div class="description-etablissement-page">
-            <p><b><o>Description de l'établissement</o></b> : Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus earum beatae consequuntur, 
-                excepturi voluptate debitis ab nulla fugiat nemo magni?
-            </p>
-        </div>
-        <div class="avis-clients-etablissement-page">
-            <p><b><o>Avis des clients</o></b> : </p>
-            <p class="p-avis-clients">Roger : "<i>Lieu de dégustation d'excellents cafés sous toutes les déclinaisons, dans un lieu aménagé avec goût.
-                Accueil chaleureux, souriant. On peut y acheter son arabica à emporter à la maison et des équipements
-                 pour faire son café. Y aller entre amis pour une pause café, y aller seul et travailler sur son labtop...
-                 tout est possible. Un lieu de tranquillité à recommander.</i>"
-            </p>
-            <p class="etoiles-moyenne-client-avis">★ ★ ★ ☆ ☆</p>
-        </div>
+<div class="body_container">
+    <div class="title-etablissement-page">
+        <h1>Nom de l'établissement</h1>
     </div>
+    <div class="img-etablissement-page">
+        <img src="./assets/img/cafe_coworking.jpg" alt="Image du café">
+    </div>
+    <div class="select-horaire-etablissement-page">
+        <label for="day-select"><b><o>Choisissez un jour :</o></b></label>
+            <select name="horaire" id="horaire">
+                <option value="lundi">Lundi 9H-17H</option>
+                <option value="mardi">Mardi 9H-17H</option>
+                <option value="mercredi">Mercredi 9H-17H</option>
+                <option value="jeudi">Jeudi 9H-17H</option>
+                <option value="vendredi">Vendredi 9H-17H</option>
+            </select>
+    </div>
+    <div class="adresse-etablissement-page">
+        <p><b><o>Adresse</o></b> : Lorem, ipsum dolor, 27140, Gisors.</p>
+    </div>
+    <div class="description-etablissement-page">
+        <p><b><o>Description de l'établissement</o></b> : Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus earum beatae consequuntur, 
+            excepturi voluptate debitis ab nulla fugiat nemo magni?
+        </p>
+    </div>
+    <div class="avis-clients-etablissement-page">
+        <p><b><o>Avis des clients</o></b> : </p>
+        <p class="p-avis-clients">Roger : "<i>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis qui autem, quam est tempora deserunt voluptatem quos sapiente consectetur eum dolore animi? Explicabo perspiciatis atque dolores incidunt? Repudiandae, vero. Nisi.</i>"
+        </p>
+        <p class="etoiles-moyenne-client-avis">★ ★ ★ ☆ ☆</p>
+    </div>
+</div>
 
 <script src="js/burger.js"></script>
 </body>
