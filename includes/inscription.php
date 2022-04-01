639c3804b9f16c6ddac423e2dcabeed8a8273565
Maël Barbé <mael.barbe47@gmail.com>
2022-04-01 10:03:48 +0200

    Création de la page inscription

---
+++ includes/inscription.php
@@ -0,0 +1,47 @@
+<div class="logo">
+    <a href="http://localhost/Projet_CoWorking/">
+        <img src="./assets/img/coffice_logo_blanc.png" alt="logo Coffice"> 
+    </a>
+</div>
+
+<header>
+    <div class="navigation-desktop">
+        <nav role="navigation">
+            <ul>
+                <li><a href="index.php?page=home">Home</a></li>
+                <li><a href="index.php?page=etablissement">Etablissement</a></li>
+                <li><a href="http://localhost/projet_coworking/index.php?page=contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<body class="resultatsPage">
+
+<nav>
+    <div class="burger-button">
+        <span class="burger-top"></span>
+        <span class="burger-middle"></span>
+        <span class="burger-bottom"></span>
+    </div>
+    <div class="burger-menu">
+        <a href="http://localhost:8888/Projet_CoWorking/index.php?page=home">Accueil</a>
+        <a href="">Se connecter</a>
+        <a href="">Ajout d'un établissement</a>
+    </div>
+</nav>
+
+<script src="js/burger.js"></script>
+
+</body>
+
+<footer>
+<div class="footer_container">
+    <div class="footer_phone">
+        <p> 📞  +33 6 12 34 56 78</p>
+    </div>
+     <div class="footer_logo-maps">
+        <img src="./assets/img/logo_google-maps.png" alt="logo google maps">
+     </div>
+</div>
+</footer>
