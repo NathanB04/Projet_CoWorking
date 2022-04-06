ef82929e1c6044dcafe6b04bb0bd204b64f98897
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-04-06 12:06:56 +0200

    creation formulaire login

--- includes/login.php
+++ includes/login.php
@@ -31,6 +31,15 @@
     </div>
 </nav>
 
+<div class="container-frm">
+<form action="index.php?page=login" method="post">
+    <ul>
+        <li><label for="mail">e-mail :</label><input type="text" id="mail" name="mail" /></li>
+        <li><label for="mdp">Mot de passe</label><input type="password" id="mdp" name="mdp" /></li>
+        <li><input type="reset" value="Effacer" /><input type="submit" value="Envoyer" name="envoi" /></li>
+    </ul>
+</form>
+</div>
 <script src="js/burger.js"></script>
 
 </body>
