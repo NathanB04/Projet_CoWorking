5d6b6941fd4a7975583aa252b002d4212cdc059b
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-04-07 17:11:04 +0200

    ajout titre page etablissement

--- includes/ajoutEtablissement.php
+++ includes/ajoutEtablissement.php
@@ -38,12 +38,14 @@
     <div class="burger-menu">
                 <a href="index.php?page=home">Accueil</a>
                 <a href="index.php?page=login">Connexion</a>
+                <a href="index.php?page=inscription">Inscription</a>
                 <a href="index.php?page=etablissement">Ajout d'un établissement</a>
                 <a href="index.php?page=resultats">Liste des établissements</a>
-                <a href="index.php?page=inscription">Inscription</a>
+                
     </div>
 </nav>
 
+<h1 class="TITLE-PAGE-ETABLISSEMENT-ADD"> AJOUT ETABLISSEMENT </h1>
 <script src="js/burger.js"></script>
 
 </body>
