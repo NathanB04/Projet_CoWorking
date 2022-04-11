243fd8fee4074fcef850d8542754d8d1e6bcce32
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-04-11 14:32:28 +0200

    essai affichage

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -3,8 +3,8 @@ include 'home.php';
 if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
     $bienvenue = "<p>";
     $bienvenue .= "Bonjour ";
-    if ($_SESSION['pseudo'] !== "") {
-        $bienvenue .= $_SESSION['pseudo'];
+    if ($_SESSION['prenom'] !== "") {
+        $bienvenue .= $_SESSION['prenom'];
     }
     else {
         $bienvenue .= $_SESSION['prenom'];
