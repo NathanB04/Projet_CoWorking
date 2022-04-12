f9f5159191e5bf3a3c125fbafad103b2f87baae7
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-04-12 11:32:53 +0200

    page test connection bdd

---
+++ includes/test.php
@@ -0,0 +1,13 @@
+<form action="index.php?page=test" method="post" enctype="multipart/form-data">
+    <ul>
+        <li><label for="nom">Nom :</label><input type="text" id="name" name="name" value="<?php echo $nom;?>" /></li>
+        <li><label for="prenom">Prénom :</label><input type="text" id="firstname" name="firstname"
+                value="<?php echo $prenom;?>" /></li>
+        <li><label for="email">e-mail :</label><input type="text" id="email" name="email"
+                value="<?php echo $email;?>" /></li>
+        <li><label for="password">Mot de passe :</label><input type="password" id="password" name="password" /></li>
+        <li><label for="passwordverif">Vérification mot de passe :</label><input type="password" id="passwordverif"
+                name="passwordverif" /></li>
+        <li><input type="reset" value="Effacer" /><input type="submit" value="S'inscrire" name="inscription" /></li>
+    </ul>
+</form>
